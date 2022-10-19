563f8c0c4ee582d3b1b5c4727c661f90fecb32e4
SungJin Yang <sungjinahim@gmail.com>
2022-10-19 22:49:33 +0900

    모달창 태그  수정

--- Traviary/src/fbase.ts
+++ Traviary/src/fbase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+// import { getFirestore } from "firebase/firestore"
 
 const {
 	VITE_FIREBASE_API_KEY,
@@ -23,5 +23,5 @@ const firebaseConfig = {
 }
 
 export const app = initializeApp(firebaseConfig)
-export const authService = getAuth()
-export const dbService = getFirestore()
+export const authService = getAuth(app)
+// export const dbService = getFirestore(app)
