fdf872f0d880b522c47be9359f85a223e0c9e741
SungJin Yang <sungjinahim@gmail.com>
2023-03-08 14:55:09 +0900

    :sparkles: Fix: 글로벌 스타일 경로 수정

--- Traviary/src/App.tsx
+++ Traviary/src/App.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import GlobalStyle from "./assets/Globalstyles";
+import GlobalStyle from "../public/assets/Globalstyles"; // 이건 베포때문에 건드리지 말것
 import Router from "./router/Router";
 import "./App.css";
 function App() {
